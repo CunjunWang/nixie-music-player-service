3611f0a6cfe5c52942c8af6eb3f2ab1a8f7ec032
Cunjun Wang <13621691063@163.com>
2019-05-04 17:30:15 +0800

    first commit

---
+++ src/main/java/com/cunjunwang/music/player/NixieMusicPlayerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cunjunwang.music.player;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NixieMusicPlayerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NixieMusicPlayerServiceApplication.class, args);
+    }
+
+}
